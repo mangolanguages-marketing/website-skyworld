a944faf596bf46dd2b63c67ec19290accc807147
jcackowski <imjuon@gmail.com>
2020-05-13 12:34:21 -0400

    megamenu languages

--- 00.Global/Header/Mega Menu/MM - Languages/megamenu.js
+++ 00.Global/Header/Mega Menu/MM - Languages/megamenu.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function (event) {
     // $('div#megamenus').appendTo('header');
 
+    var myStylesLocation = '/userfiles/files/css/megamenu/megamenu.css';
+    $('<link rel="stylesheet" type="text/css" href="' + myStylesLocation + '" >').appendTo('head');
+
     $.get('/userfiles/files/css/megamenu/megamenu.html', function (data) {
         $('header').append(data);
     });
@@ -10,16 +13,18 @@ document.addEventListener('DOMContentLoaded', function (event) {
     }, function () {
         $('.megamenu--languages').removeClass('active');
     });
+
+});
+
+window.addEventListener('load', function () {
     $('.megamenu--languages li a').each(function (index) {
         $(this).on("click", function () {
+            console.log($(this).text());
             ga('gtag_UA_1350365_39.send', 'event', {
-                'eventCategory': 'megaheader Language',
-                'eventAction': 'Language',
+                'eventCategory': 'Header Megamenu link',
+                'eventAction': 'Language link',
                 'eventLabel': $(this).text()
             });
         });
     });
-    var myStylesLocation = '/userfiles/files/css/megamenu/megamenu.css';
-    $('<link rel="stylesheet" type="text/css" href="' + myStylesLocation + '" >').appendTo('head');
 });
-
